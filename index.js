498f8e934b607f81c448fdc4c6487e504aeb6bb3
Brian Pattison <brian@brianpattison.com>
2018-03-02 10:36:58 -0600

    Add sort-class-members rules

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 module.exports = {
-  env: {
-    browser: true
+  "env": {
+    "browser": true
   },
-  extends: ["xo", "plugin:unicorn/recommended", "prettier", "prettier/react"],
-  globals: {
-    afterAll: true,
-    afterEach: true,
-    beforeAll: true,
-    beforeEach: true,
-    describe: true,
-    expect: true,
-    it: true,
-    jest: true
+  "extends": [
+    "xo",
+    "plugin:unicorn/recommended",
+    "prettier",
+    "prettier/react"
+  ],
+  "globals": {
+    "afterAll": true,
+    "afterEach": true,
+    "beforeAll": true,
+    "beforeEach": true,
+    "describe": true,
+    "expect": true,
+    "it": true,
+    "jest": true
   },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     }
   },
-  plugins: ["import", "prettier", "react", "unicorn"],
-  rules: {
+  "plugins": [
+    "import",
+    "prettier",
+    "react",
+    "sort-class-members",
+    "unicorn"
+  ],
+  "rules": {
     "capitalized-comments": "off",
-    eqeqeq: [
+    "eqeqeq": [
       "error",
       "always",
       {
-        null: "never"
+        "null": "never"
       }
     ],
     "guard-for-in": "off",
@@ -38,23 +49,53 @@ module.exports = {
     "no-unused-vars": [
       "error",
       {
-        varsIgnorePattern: "React"
+        "varsIgnorePattern": "React"
       }
     ],
     "prettier/prettier": [
       "error",
       {
-        printWidth: 120
+        "printWidth": 120
       }
     ],
-    radix: ["error", "as-needed"],
+    "radix": [
+      "error",
+      "as-needed"
+    ],
     "react/jsx-no-literals": 1,
     "react/jsx-uses-vars": 1,
+    "sort-class-members/sort-class-members": [
+      2,
+      {
+        "groups": {
+          "other-methods": [
+            {
+              "name": "/^(?!render|constructor).+/",
+              "sort": "alphabetical",
+              "type": "method"
+            }
+          ],
+          "render-methods": [
+            {
+              "name": "/render.+/",
+              "sort": "alphabetical",
+              "type": "method"
+            }
+          ]
+        },
+        "order": [
+          "constructor",
+          "[other-methods]",
+          "[render-methods]"
+        ],
+        "stopAfterFirstProblem": true
+      }
+    ],
     "valid-jsdoc": [
       "error",
       {
-        requireReturn: false,
-        requireReturnDescription: false
+        "requireReturn": false,
+        "requireReturnDescription": false
       }
     ]
   }
